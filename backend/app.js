b3f56d8432c3cbc5fac1211f24928d150a5f5732
Baekshook <ecgo0106@naver.com>
2023-04-13 14:36:38 +0900

    백엔드 초기 세팅 (prisma, cors 설치)

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const port = 3010;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port : ${port} 🦉`);
+});
