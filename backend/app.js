a6c7de26b846e1c8b4d9ab6b87205a2b78a53d95
Baekshook <ecgo0106@naver.com>
2023-04-14 09:37:17 +0900

    DB 연결 초기 세팅

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 
+const userRouter = require("./routes/user");
+
 const app = express();
 
-const port = 3010;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, Express!");
 });
-
 app.listen(port, () => {
-  console.log(`Server listening on port : ${port} 🦉`);
+  console.log(`Server listening on port : ${port} 🚀`);
 });
