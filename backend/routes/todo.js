9d3345247d38541b929160d159d76d15f1ef4bb1
Baekshook <ecgo0106@naver.com>
2023-04-14 11:46:10 +0900

    todo 조회 기능 구현

--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -30,17 +30,44 @@ router.post("/", async (req, res) => {
     const newTodo = await client.todo.create({
       data: {
         todo,
-        idDone: false,
+        isDone: false,
         userId: user.id,
       },
     });
 
-    res.json({ok: false, todo: newTodo});
+    res.json({ ok: true, todo: newTodo });
   } catch (error) {
     console.error(error);
   }
 });
+
 // 투두 조회
+router.get("/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const user = await client.user.findUnique({
+      where: {
+        id: parseInt(userId),
+      },
+    });
+
+    if (!user) {
+      return res.status(400).json({ ok: false, error: "Not exist user." });
+    }
+
+    const todos = await client.todo.findMany({
+      where: {
+        userId: parseInt(userId),
+      },
+    });
+
+    res.json({ ok: true, todos });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
 // 투두 완료
 // 투두 삭제
 
