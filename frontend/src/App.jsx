630a632f94ad86cdd1e19af4d7307266784ed0f9
Baekshook <ecgo0106@naver.com>
2023-04-17 11:12:31 +0900

    Logout 기능 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,25 @@ import Login from "./components/Login";
 function App() {
   const [user, setUser] = useState();
 
+  const onClickLogOut = () => {
+    setUser(undefined);
+  };
+
   if (!user) {
-    return <Login setUser={setUser}/>;
+    return <Login setUser={setUser} />;
   }
 
   return (
     <div className="min-h-screen flex flex-col justify-start items-center pt-16">
-      <h1 className="text-4xl font-bold">{user.account}님 환영합니다! 😎</h1>
+      <h1 className="text-4xl font-bol flex items-center">
+        {user.account}님 환영합니다! 😎
+        <button
+          className="ml-4 px-2 py-1 hover:bg-pink-600 bg-pink-400 rounded-lg text-gray-50 text-base"
+          onClick={onClickLogOut}
+        >
+          로그아웃
+        </button>
+      </h1>
       <div>
         <div className="mt-8 text-sm font-semibold">
           If I only had an hour to chop down a tree, I would spend the first 45
