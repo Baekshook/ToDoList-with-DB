f6b740ccd1db43f770c7e482badd69cd9b16637c
Baekshook <ecgo0106@naver.com>
2023-04-17 11:35:42 +0900

    TodoCard 코드 간소화

--- frontend/src/components/TodoCard.jsx
+++ frontend/src/components/TodoCard.jsx
@@ -10,14 +10,11 @@ export default function TodoCard(props) {
               <div className="border-4 border-pink-400 w-8 h-8 rounded-xl bg-pink-400 p-2"></div>
               <div className="absolute border-4 border-white top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 w-8 h-8 scale-75 rounded-xl bg-pink-400 p-2"></div>
             </div>
-            <div className="text-2xl ml-4 truncate">{props.todo}</div>
           </>
         ) : (
-          <>
-            <div className="border-4 border-pink-400 w-8 h-8 rounded-xl"></div>
-            <div className="text-2xl ml-4 truncate">{props.todo}</div>
-          </>
+          <div className="border-4 border-pink-400 w-8 h-8 rounded-xl"></div>
         )}
+        <div className="text-2xl ml-4 truncate">{props.todo}</div>
       </div>
     </>
   );
